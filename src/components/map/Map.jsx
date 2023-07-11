62ea561ba4af103a7b68ee6e00a88eaa71ff0077
Azeem Adenuga <adenugaazeem80@gmail.com>
2023-07-11 11:02:13 +0100

    map integration and bug fixes

--- src/components/map/Map.jsx
+++ src/components/map/Map.jsx
@@ -1,28 +1,60 @@
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { useEffect, useRef } from "react";
 import { generateCoordinates } from "./function";
+import * as am5 from "@amcharts/amcharts5";
+import * as am5map from "@amcharts/amcharts5/map";
+import am5themes_Animated from "@amcharts/amcharts5/themes/Animated";
+import am5geodata_worldLow from "@amcharts/amcharts5-geodata/worldLow";
 
 export const Map = () => {
-  const coordinatesArray = generateCoordinates();
+  useEffect(() => {
+    const data = generateCoordinates();
+    let root = am5.Root.new("chart-div");
 
-  return (
-    <MapContainer
-      center={[0, 0]}
-      zoom={2}
-      style={{ height: "100%", width: "100%" }}
-    >
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution="Map data &copy; <a href='https://www.openstreetmap.org/'>OpenStreetMap</a> contributors"
-      />
+    root.setThemes([am5themes_Animated.new(root)]);
 
-      {coordinatesArray.map((coordinate, index) => (
-        <Marker
-          key={index}
-          position={[coordinate.latitude, coordinate.longitude]}
-        >
-          <Popup>Price: ${coordinate.price}</Popup>
-        </Marker>
-      ))}
-    </MapContainer>
-  );
+    let chart = root.container.children.push(
+      am5map.MapChart.new(root, {
+        panX: "rotateX",
+        panY: "none",
+        projection: am5map.geoNaturalEarth1(),
+        homeZoomLevel: 3.5,
+      })
+    );
+
+    // Create polygon series
+    let polygonSeries = chart.series.push(
+      am5map.MapPolygonSeries.new(root, {
+        geoJSON: am5geodata_worldLow,
+        exclude: ["AQ"],
+      })
+    );
+    let pointSeries = chart.series.push(
+      am5map.MapPointSeries.new(root, {
+        latitudeField: "latitude",
+        longitudeField: "longitude",
+      })
+    );
+    pointSeries.bullets.push(function () {
+      let circle = am5.Circle.new(root, {
+        radius: 5,
+        fill: am5.color(0xff0000),
+        tooltipText: "${price}",
+      });
+
+      circle.states.create("hover", {
+        fill: am5.color(0x00ff00),
+      });
+
+      return am5.Bullet.new(root, {
+        sprite: circle,
+      });
+    });
+    pointSeries.data.setAll(data);
+
+    return () => {
+      root.dispose();
+    };
+  }, []);
+
+  return <div id="chart-div" style={{ width: "100%", height: "500px" }}></div>;
 };
